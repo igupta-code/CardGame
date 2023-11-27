b3c5bee42cd1a48d5e6417ae3d3b3aa9bb80a306
ishagupta <isha.gupta@menloschool.org>
2023-11-27 13:49:52 -0800

    Created constructor

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Card {
+public class Card {
+    private int point;
+    private String suit;
+    private String rank;
+
+    public Card(String suit, String rank, int point){
+        this.suit = suit;
+        this.rank = rank;
+        this.point = point;
+
+    }
+
+
 }
